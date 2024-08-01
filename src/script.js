78cd09ca369a23b47a4a59712d8fa1793aefdcf2
Jaz-spec <jasminemaslen7@gmail.com>
2024-08-01 08:25:03 +0100

    adding API functionality

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
-// search engine function
+let searchElement = document.querySelector("#search");
+searchElement.addEventListener("submit", handleSearch);
 
-function searchEngine(event) {
+// search engine function
+function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchInput");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+
+  getData(searchInput.value);
 }
 
-let searchElement = document.querySelector("#search");
-searchElement.addEventListener("submit", searchEngine);
+//retreaiving API
+function getData(city) {
+  let apiKey = "oa9f439cb230f940atf8b1fac2e41075";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(url).then(refreshWeather);
+}
+
+//updating details
+function refreshWeather(response) {
+  console.log(response);
+}
